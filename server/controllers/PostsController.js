495b4308bbc07ef11012738084eaee7a6224cb1b
Prasundas99 <prasundas115@gmail.com>
2024-10-20 00:53:45 +0530

    pagination apis initialized

---
+++ server/controllers/PostsController.js
@@ -0,0 +1,73 @@
+import Post from "../model/postSchema.js";
+
+export const addPostsController = async (req, res) => {
+  const { title, content } = req.body;
+
+  if (!title || !content) {
+    return res.status(400).json({ message: "Title and content are required" });
+  }
+
+  const newPost = new Post({ title, content });
+  await newPost.save();
+  res.status(200).json({ message: "Post added successfully" });
+};
+
+export const getInfiniteScrollController = async (req, res) => {
+  const limit = parseInt(req.query.limit) || 10;
+  const lastItemId = req.query.lastItemId;
+
+  try {
+    let query = {};
+
+    if (lastItemId) {
+      query = { _id: { $gt: lastItemId } };
+    }
+
+    const posts = await Post.find(query)
+      .sort({ title: 1})
+      .limit(limit)
+      .lean();
+
+    res.status(200).json({
+      message: "Posts fetched successfully",
+      data: {
+        posts,
+        lastItemId: posts[posts.length - 1]._id,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getPaginationController = async (req, res) => {
+  const limit = parseInt(req.query.limit) || 10;
+  const page = parseInt(req.query.page) || 1;
+  const skip = (page - 1) * limit;
+
+  try {
+    const total = await Post.countDocuments();
+
+    const posts = await Post.find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .lean();
+
+    const totalPages = Math.ceil(total / limit);
+
+    res.status(200).json({
+      message: "Posts fetched successfully",
+      data: {
+        posts,
+        total,
+        totalPages: totalPages,
+        currentPage: page,
+        hasNextPage: page < totalPages,
+        hasPreviousPage: page > 1,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express" 
+import cors from "cors"
+import dbConnection from "./config/dbConnection.js"
+import { addPostsController, getInfiniteScrollController, getPaginationController } from "./controllers/PostsController.js"
+
+const app  = express()
+const PORT = process.env.PORT || 5000
+
+dbConnection()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cors())
+
+//routes
+app.post("/add-post", addPostsController)
+app.get("/curser-based-pagination", getInfiniteScrollController)
+app.get("/offset-based-pagination", getPaginationController)
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
